46f54cbf9b89b94693ff2147b3911c14d4c12ea6
arkanmaho <33277200+arkanmaho@users.noreply.github.com>
2018-01-12 09:21:20 +0800

    database2

--- app/src/main/java/com/example/konong/calender2/MainActivity.java
+++ app/src/main/java/com/example/konong/calender2/MainActivity.java
@@ -9,10 +9,12 @@ import android.widget.LinearLayout;
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton button1,button2,button3;
     LinearLayout layout1,layout2;
+    DBHelper dbku;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbku=new DBHelper(this);
         button1=(FloatingActionButton) findViewById(R.id.but1);
         button2=(FloatingActionButton) findViewById(R.id.ulang);
         button3=(FloatingActionButton) findViewById(R.id.ulang1);
---
+++ app/src/main/java/com/example/konong/calender2/mindb.java
@@ -0,0 +1,8 @@
+package com.example.konong.calender2;
+
+/**
+ * Created by Konong on 2018-01-11.
+ */
+
+public class mindb {
+}
