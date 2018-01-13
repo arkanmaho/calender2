d496b809f5236c1134f500d275c08967e5424e74
arkanmaho <33277200+arkanmaho@users.noreply.github.com>
2018-01-13 08:27:36 +0800

    button/layout

--- app/src/main/java/com/example/konong/calender2/MainActivity.java
+++ app/src/main/java/com/example/konong/calender2/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.konong.calender2;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton button1,button2,button3;
     LinearLayout layout1,layout2;
     DBHelper dbku;
+    public String date,start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbku=new DBHelper(this);
+        final TextView ttl=(TextView) findViewById(R.id.total);
+        CalendarView cv=(CalendarView)findViewById(R.id.callll);
         button1=(FloatingActionButton) findViewById(R.id.but1);
         button2=(FloatingActionButton) findViewById(R.id.ulang);
         button3=(FloatingActionButton) findViewById(R.id.ulang1);
@@ -33,5 +40,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date=+year+"-"+(month+1)+"-"+dayOfMonth;
+               // ttl.setText(date);
+            }
+        });
+    }
+
+    public void repeat(View view) {
+        Intent intent=new Intent(MainActivity.this, berulang.class);
+        intent.putExtra("tanggal",date);
+        startActivity(intent);
+    }
+
+    public void langsung(View view) {
+        Intent intent=new Intent(MainActivity.this, langsung.class);
+        intent.putExtra("tanggal",date);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/konong/calender2/mindb.java
+++
@@ -1,8 +0,0 @@
-package com.example.konong.calender2;
-
-/**
- * Created by Konong on 2018-01-11.
- */
-
-public class mindb {
-}
